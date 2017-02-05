ed0b83fd672c533c8fc73e8bf7fb7700f9710e40
madhat2r <MaDhAt2r@dukefoo.com>
2017-02-04 21:29:24 -0600

    wonderland number solution

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,14 @@
 (ns wonderland-number.finder)
 
+(defn test-num [x]
+  (let [t (for [y (range 2 7)]
+            (* y x))
+        s (map #(set (str %)) t)]
+    (apply = s)))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+    (loop [r (range 100000 999999)]
+        (cond
+          (empty? r) nil
+          (test-num (first r)) (first r)
+          :else (recur (rest r)))))
